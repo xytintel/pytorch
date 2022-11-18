3025b5dac16f162015346ebe8e0bf9f690ab6e55
Horace He <chilli@fb.com>
2022-11-18 21:39:12 +0000

    Don't allow recomputing a node that *must* be materialized in the backwards pass
    ghstack-source-id: 2430083fa777e5cb00716653a9236ffd061356a2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89171

--- functorch/_src/partitioners.py
+++ functorch/_src/partitioners.py
@@ -284,8 +284,9 @@ def min_cut_rematerialization_partition(
     fx_g = joint_module.graph
 
     #  add the CSE pass
-    cse_graph = fx_graph_cse(fx_g)
-    joint_module.graph = cse_graph
+    if config.cse:
+        cse_graph = fx_graph_cse(fx_g)
+        joint_module.graph = cse_graph
     full_bw_graph = joint_module.graph
 
     name_to_node = {}
@@ -361,7 +362,20 @@ def min_cut_rematerialization_partition(
                 return False
             if node.target in [aten.lift_fresh_copy.default, aten.lift_fresh.default]:
                 return False
-            if compiler == "inductor" and node.dist_from_bw > 3:
+
+            # If a node *must* be materialized in the backwards pass, then we
+            # should never recompute it. This is a pretty subtle point.  In
+            # general, the assumption we make is that recomputing a node in the
+            # backwards pass is "free". However, if a node must be materialized
+            # in the backwards pass, then recomputing it is never free.
+            for user in node.users:
+                if user in required_bw_nodes and not is_fusible(node, user):
+                    return True
+
+            # Arbitrary hack that sometimes seems to help things. The above
+            # modification appears to have made this heuristic a lot less critical
+            # for performance.
+            if compiler == "inductor" and node.dist_from_bw > config.max_dist_from_bw:
                 return True
             # If the output of an op is 4x smaller (arbitrary choice),
             # then we don't allow recomputation.
